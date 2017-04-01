bb974812c53d9cc28886784954a0fbfc7b0c3777
William Hornsby <hornsbywilliamd@gmail.com>
2017-04-01 03:36:58 +0000

    float?

--- EloRatings.py
+++ EloRatings.py
@@ -44,8 +44,8 @@ class Player:
         return (hypothetialRating, hypothetialOther)
 
     def chance(self, other):
-        expected = 1.0 / (1.0 + math.pow(10, (other.rating - self.rating) / 400.0))
-        otherExpected = 1.0 / (1.0 + math.pow(10, (self.rating - other.rating) / 400.0))
+        expected = float(1.0 / (1.0 + math.pow(10, (other.rating - self.rating) / 400.0)))
+        otherExpected = float(1.0 / (1.0 + math.pow(10, (self.rating - other.rating) / 400.0)))
         return (expected, otherExpected)
 
     def __str__(self):
